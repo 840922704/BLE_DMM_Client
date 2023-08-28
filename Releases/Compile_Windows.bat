15b8a0d7cc2c7fef6f2d37a8875d91fa1d99d57a
840922704 <840922704@qq.com>
2023-08-28 22:42:18 +0800

    V3.2 Release

--- Releases/Compile_Windows.bat
+++ Releases/Compile_Windows.bat
@@ -1,11 +1,11 @@
-@echo off
-echo Nuitka Single File
+echo Entering Pyside6-rcc Resources Compile
+pyside6-rcc ../Ui/resources.qrc -o ../Ui/resources_rc.py
+
+echo Entering Nuitka Single File Compile
 python -m nuitka --standalone --windows-disable-console --enable-plugin=pyside6 --show-progress --windows-icon-from-ico="..\Ui\Logo.ico" --include-package-data=qt_material --output-dir="./Single" --onefile ../main.py
 
-echo Nuitka Single File Debug
+echo Entering Nuitka Single File Debug Compile
 python -m nuitka --standalone --enable-plugin=pyside6 --show-progress --windows-icon-from-ico="..\Ui\Logo.ico" --include-package-data=qt_material --output-dir="./Single_Debug" --onefile ../main.py
 
-echo Nuitka Normal
+echo Entering Nuitka Normal Compile
 python -m nuitka --standalone --windows-disable-console --enable-plugin=pyside6 --show-progress --windows-icon-from-ico="..\Ui\Logo.ico" --include-package-data=qt_material --output-dir="./Normal" ../main.py
-
-pause
